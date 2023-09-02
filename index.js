ba5814379e60ae3942b625dcf1559404a2643f2f
Cwdrian <xcharlesdrian@gmail.com>
2023-09-02 00:42:05 -0400

    completed

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ move(inventory).to(0, 0)
 const character = newImage('assets/green-character/static.gif')
 let direction = null;
 let x = 100;
-let y = 250;
+let y = 250; 
+
 
 //function to move character
 
@@ -24,7 +25,7 @@ if(direction === 'south'){
 character.style.left = x + 'px'
 character.style.bottom = y + 'px'
 } , 1)
-move(character).to(100, 250)
+move(character).withArrowKeys(100, 250, handleDirectionChange)
 
 //move with arrow keys
 
@@ -43,13 +44,34 @@ document.addEventListener('keydown', function(e){
     if(e.key === 'ArrowDown'){
         direction = 'south'
     }
-})
+}) 
 
 //stops character from moving when key is released
 
 document.addEventListener('keyup', function(e){
     direction = null
 })
+//character movement 
+
+function handleDirectionChange(direction){
+if(direction === null){
+    character.src = 'assets/green-character/static.gif'
+}
+if(direction === 'west'){
+    character.src = 'assets/green-character/west.gif'
+}
+if(direction === 'north'){
+    character.src = 'assets/green-character/north.gif'
+}
+if(direction === 'east'){
+    character.src = 'assets/green-character/east.gif'
+}
+if(direction === 'south'){
+    character.src = 'assets/green-character/south.gif'
+}
+}
+
+
 
 
 move(newImage('assets/tree.png')).to(200, 450)
